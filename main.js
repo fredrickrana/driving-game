96f5aa44a71cec60620e2dbd7d92ce95cc67cb4a
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-29 20:23:01 +0000

    change racecar positioning to absolute. utilize setInterval method to continously change positioning of car. clear interval when car reaches end of page.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var $left = 37;
 var $up = 38;
 var $right = 39;
 var $down = 40;
+var $space = 32;
+var $startLine = 0;
 
 function rotateCar(event) {
   if (event.keyCode === $down) {
@@ -16,3 +18,17 @@ function rotateCar(event) {
   }
 }
 window.addEventListener('keydown', rotateCar);
+
+function gas(event) {
+  if (event.keyCode === $space) {
+    var $go = setInterval(function () {
+      if ($startLine > 2000) {
+        clearInterval($go);
+      } else {
+        $startLine += 3;
+        $raceCar.style.left = $startLine + 'px';
+      }
+    });
+  }
+}
+window.addEventListener('keydown', gas);
