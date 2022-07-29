3332388070829fbe42af645b13a299e88d19b4a9
Fredrick Rana <fredrickrana01@gmail.com>
2022-07-29 19:03:22 +0000

    created a main.js file. added new css rulesets to match position of racecar. created a function which changed racecar class to the specified css ruleset. added an eventlistener to window.

---
+++ main.js
@@ -0,0 +1,20 @@
+// keydown event would change class to rotate each direction;
+
+var $raceCar = document.querySelector('.race-car');
+var $left = 37;
+var $up = 38;
+var $right = 39;
+var $down = 40;
+
+function rotateCar(event) {
+  if (event.keyCode === $down) {
+    $raceCar.className = 'race-car rotate-down';
+  } else if (event.keyCode === $up) {
+    $raceCar.className = 'race-car rotate-up';
+  } else if (event.keyCode === $left) {
+    $raceCar.className = 'race-car rotate-left';
+  } else if (event.keyCode === $right) {
+    $raceCar.className = 'race-car rotate-right';
+  }
+}
+window.addEventListener('keydown', rotateCar);
